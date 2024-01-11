acce30dd8ec406a783d25b59545f85f4fd18b871
shakhal350 <91167975+shakhal350@users.noreply.github.com>
2024-01-11 16:48:40 -0500

    Updated the main_raw_testing.py & data_processing.py to read the Github Datasets.

--- Python Simulation/data_processing.py
+++ Python Simulation/data_processing.py
@@ -2,7 +2,16 @@ import pandas as pd
 
 
 def load_and_process_data(filename):
-    data = pd.read_csv(filename, header=None)
-    for col in data.columns:
-        data[col] = data[col].apply(lambda x: complex(x.replace('i', 'j')))
-    return data
+    # For Children Dataset
+    if 'Transposed' in filename:
+        data = pd.read_csv(filename, header=None)
+        for col in data.columns:
+            data[col] = data[col].apply(lambda x: complex(x.replace('i', 'j')))
+        data_avg = data[0] + data[1] + data[2] + data[3] / 4  # Average all 4 Rx Antennas
+        return data_avg
+
+    # For VitalSign Dataset from Github
+    if 'mmWave-VitalSign' in filename:
+        data = pd.read_csv(filename, header=None)
+        data = data[1]  # Only use one Rx Antenna (uses only Absolute Values)
+        return data
--- Python Simulation/main_raw_testing.py
+++ Python Simulation/main_raw_testing.py
@@ -1,6 +1,5 @@
 from data_processing import load_and_process_data
 from plotting import setup_plots, create_animation
-from range_FFT import animate_range_fft
 
 # Radar parameters
 AntNum = 4  # Number of Rx Antennas
@@ -21,24 +20,26 @@ FFTSize = 2 ** 10  # FFT Size for Range-FFT
 RangeBin = (c * samplerate) / (2 * freqslope * FFTSize)  # Range Resoluion of Range-FFT
 
 # Parameters and filename
-filename1 = r'C:\Users\Shaya\Documents\MATLAB\CAPSTONE DATASET\CAPSTONE DATASET\Children Dataset\FMCW Radar\Rawdata\Transposed_Rawdata\Transposed_Rawdata_1.csv'
-# filename2 = r'C:\Users\Shaya\CAPSTONE\VitalSign-Capstone-2023\DSP_vital_signs\recordings\data1.csv'
-sample_window_size = 10240
-cutoff_threshold = 1000
+# filename = r'C:\Users\Shaya\Documents\MATLAB\CAPSTONE DATASET\CAPSTONE DATASET\Children Dataset\FMCW Radar\Rawdata\Transposed_Rawdata\Transposed_Rawdata_1.csv'
+# sample_window_size = 10240
+# cutoff_threshold = 1000
+# update_interval = 1
+
+filename = r'C:\Users\Shaya\PycharmProjects\VitalSign-Capstone-2023\mmWave-VitalSign (Dataset Github)\RobustVSDataset_anonymous\p1\fix\2m\periodical\radar_csv\radar_01.csv'
+sample_window_size = 200
+cutoff_threshold = 50
 update_interval = 1
 
 # Load and process data
-data = load_and_process_data(filename1)
+data = load_and_process_data(filename)
 
-data_avg = (data[0] + data[1] + data[2] + data[3]) / 4
-
-animate_range_fft(data_avg, FFTSize, RangeBin)
+print(data)
 
 # Setup plots
 fig, ax1, ax2, ax3, ax4, line1, line2, line3, line4 = setup_plots()
 
 # Create and start animation
-create_animation(fig, data_avg, samples_per_frame, fps, sample_window_size, update_interval, cutoff_threshold, line1,
+create_animation(fig, data, samples_per_frame, fps, sample_window_size, update_interval, cutoff_threshold, line1,
                  line2,
                  line3, line4, ax1,
                  ax2,
