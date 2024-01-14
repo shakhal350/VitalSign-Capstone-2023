d00c95ada808562cf34f46c6dcbcbcb1f8cf771d
shakhal350 <91167975+shakhal350@users.noreply.github.com>
2024-01-14 14:25:17 -0500

    Shortened version of main due to changes in plotting.py

--- Python Simulation/main_raw_testing.py
+++ Python Simulation/main_raw_testing.py
@@ -2,12 +2,12 @@ import numpy as np
 
 from SVD_processing import SVD_Matrix, reduce_noise
 from data_processing import load_and_process_data
-from plotting import setup_plots, create_animation
+from plotting import setup_plots_time_fft, create_animation
 
 # Parameters and filename
-filename = r'C:\Users\Shaya\Documents\MATLAB\CAPSTONE DATASET\CAPSTONE DATASET\Children Dataset\FMCW Radar\Rawdata\Transposed_Rawdata\Transposed_Rawdata_1.csv'
-sample_window_size = 1024
-update_interval = 0.01
+filename = r'C:\Users\Shaya\Documents\MATLAB\CAPSTONE DATASET\CAPSTONE DATASET\Children Dataset\FMCW Radar\Rawdata\Transposed_Rawdata\Transposed_Rawdata_11.csv'
+sample_window_size = 40960
+update_interval = 2
 
 # filename = r'C:\Users\Shaya\PycharmProjects\VitalSign-Capstone-2023\mmWave-VitalSign (Dataset Github)\RobustVSDataset_anonymous\p1\fix\2m\periodical\radar_csv\radar_01.csv'
 # sample_window_size = 50
@@ -22,12 +22,12 @@ SVD_U, SVD_s, SVD_Vh = SVD_Matrix(np.abs(data), radar_parameters)
 data = reduce_noise(SVD_U, SVD_s, SVD_Vh, num_components=5)
 
 # Setup plots
-fig, ax1, ax2, ax3, ax4, line1, line2, line3, line4 = setup_plots()
+fig, ax1, ax2, line1, line2 = setup_plots_time_fft()
 
 # Create and start animation
 create_animation(fig, data, radar_parameters["samplesPerFrame"], radar_parameters["frameRate"], sample_window_size,
                  update_interval, line1,
                  line2,
-                 line3, line4, ax1,
+                 ax1,
                  ax2,
-                 ax3, ax4)
+                 )
