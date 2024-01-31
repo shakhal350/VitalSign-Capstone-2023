41d2a3fb4472ce625de8c69872f0dd393f6e9454
Liam Pereir <pereira.l.h@hotmail.ca>
2024-01-31 10:43:02 -0500

    Load transposed raw data 11 in Python Simulation

--- PythonSimulation/main_raw_testing.py
+++ PythonSimulation/main_raw_testing.py
@@ -5,14 +5,14 @@ from data_processing import load_and_process_data
 from plotting import create_animation
 
 # Parameters and filename
-filename = r'C:\Users\Shaya\Documents\MATLAB\CAPSTONE DATASET\CAPSTONE DATASET\Children Dataset\FMCW Radar\Rawdata\Transposed_Rawdata\Transposed_Rawdata_11.csv'
+filename = r'/Users/liampereira/Documents/Code/VitalSign-Capstone-2023/PythonSimulation/Transposed_Rawdata_11.csv'
+
 sample_window_size = 40960
 update_interval = 2
 
 # filename = r'C:\Users\Shaya\PycharmProjects\VitalSign-Capstone-2023\mmWave-VitalSign (Dataset Github)\RobustVSDataset_anonymous\p1\fix\2m\periodical\radar_csv\radar_01.csv'
 # sample_window_size = 50
 # update_interval = 0.1
-
 
 # Load and process data
 data, radar_parameters = load_and_process_data(filename)
@@ -21,7 +21,7 @@ print(radar_parameters["samplesPerFrame"], radar_parameters["frameRate"])
 SVD_U, SVD_s, SVD_Vh = SVD_Matrix(np.abs(data), radar_parameters)
 data = reduce_noise(SVD_U, SVD_s, SVD_Vh, num_components=5)
 
-# Create and start animation
-create_animation(data, radar_parameters["samplesPerFrame"], radar_parameters["frameRate"], sample_window_size,
-                 update_interval,
-                 )
+# # Create and start animation
+# create_animation(data, radar_parameters["samplesPerFrame"], radar_parameters["frameRate"], sample_window_size,
+#                  update_interval,
+#                  )
