1fcfebf133b2c3817809508a88aaad411b899e0c
tchardino <jonathan.tcharfajian@gmail.com>
2024-03-18 23:38:12 -0400

    Small GUI Changes

--- PythonSimulation/process_raw_data.py
+++ PythonSimulation/process_raw_data.py
@@ -19,12 +19,12 @@ def readDCA1000(fileName, csvFileName):
     with open(fileName, 'rb') as f:
         adcData = np.fromfile(f, dtype=np.int16)
 
-    # # Compensate for sign extension if ADC bits are not 16
+    # Compensate for sign extension if ADC bits are not 16
     if numADCBits != 16:
         l_max = 2 ** (numADCBits - 1) - 1
         adcData[adcData > l_max] -= 2 ** numADCBits
 
-    # # Organize data by LVDS lane
+    # Organize data by LVDS lane
     if isReal:
         # Reshape data based on one sample per LVDS lane for real only data
         adcData = np.reshape(adcData, (numLanes, -1))
@@ -33,13 +33,13 @@ def readDCA1000(fileName, csvFileName):
         adcData = np.reshape(adcData, (-1, numLanes * 2))
         adcData = adcData[:, :numLanes] + 1j * adcData[:, numLanes:]
 
-    # # Convert to DataFrame and save to CSV
-    # # No need to transpose, as we've already arranged the array correctly
+    # Convert to DataFrame and save to CSV
+    # No need to transpose, as we've already arranged the array correctly
     df = pd.DataFrame(adcData)
-    # # Format each cell in DataFrame using the complex_formatter function
+    # Format each cell in DataFrame using the complex_formatter function
     formatted_df = df.map(complex_formatter)
 
-    # # Save the formatted DataFrame to CSV without index
+    # Save the formatted DataFrame to CSV without index
     formatted_df.to_csv(csvFileName, index=False)
 
     return adcData
