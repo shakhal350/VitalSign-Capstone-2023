c404c9792a63e388dcf73f68db28e956d926796a
shakhal350 <91167975+shakhal350@users.noreply.github.com>
2024-03-18 23:47:17 -0400

    Added DummySerialPort.py for testing code without Radar connected

--- PythonSimulation/process_raw_data.py
+++ PythonSimulation/process_raw_data.py
@@ -15,16 +15,15 @@ def readDCA1000(fileName, csvFileName):
     isReal = 0  # set to 1 if real only data, 0 if complex data
 
     # Read .bin file
-    print("Reading .bin file")
     with open(fileName, 'rb') as f:
         adcData = np.fromfile(f, dtype=np.int16)
 
-    # # Compensate for sign extension if ADC bits are not 16
+    # Compensate for sign extension if ADC bits are not 16
     if numADCBits != 16:
         l_max = 2 ** (numADCBits - 1) - 1
         adcData[adcData > l_max] -= 2 ** numADCBits
 
-    # # Organize data by LVDS lane
+    # Organize data by LVDS lane
     if isReal:
         # Reshape data based on one sample per LVDS lane for real only data
         adcData = np.reshape(adcData, (numLanes, -1))
@@ -33,16 +32,16 @@ def readDCA1000(fileName, csvFileName):
         adcData = np.reshape(adcData, (-1, numLanes * 2))
         adcData = adcData[:, :numLanes] + 1j * adcData[:, numLanes:]
 
-    # # Convert to DataFrame and save to CSV
-    # # No need to transpose, as we've already arranged the array correctly
+    # Convert to DataFrame and save to CSV
+    # No need to transpose, as we've already arranged the array correctly
     df = pd.DataFrame(adcData)
-    # # Format each cell in DataFrame using the complex_formatter function
+    # Format each cell in DataFrame using the complex_formatter function
     formatted_df = df.map(complex_formatter)
 
-    # # Save the formatted DataFrame to CSV without index
+    # Save the formatted DataFrame to CSV without index
     formatted_df.to_csv(csvFileName, index=False)
-
+    print(f"Data saved to {csvFileName}")
     return adcData
 
 
-#readDCA1000(r"C:\ti\mmwave_studio_02_01_01_00\mmWaveStudio\PostProc\adc_data.bin", r"C:\Users\Shaya\PycharmProjects\VitalSign-Capstone-2023\DATASET\DCA1000EVM_Shayan_normal_upclose_60sec.csv")  # Enter file path
+readDCA1000(r"C:\ti\mmwave_studio_02_01_01_00\mmWaveStudio\PostProc\adc_data.bin", r"C:\Users\Shaya\OneDrive - Concordia University - Canada\UNIVERSITY\CAPSTONE\Our Datasets (DCA1000EVM)\CSVFiles(RawData)\ChestDisplacementData\March13th_Shayan_test\3\DCA1000EVM_Shayan_Shower_20Br_140Hr.csv")  # Enter file path
